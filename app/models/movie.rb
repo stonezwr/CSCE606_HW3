7e310bc35b25741893e36a334857173e4a4ae89e
Wenrui Zhang <wenrui.z1993@gmail.com>
2016-10-14 04:29:21 +0000

    part2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+        ar= Array.new
+        self.select("rating").uniq.each {|x| ar.push(x.rating)}
+        ar.sort.uniq
+    end
 end
